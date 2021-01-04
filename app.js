f7b1f92548331809330252996832e150095010d3
Melvin Sierra <msierrawd@gmail.com>
2021-01-04 17:10:14 -0500

    Adding content

--- app.js
+++ app.js
@@ -1,1 +1,52 @@
-ReactDOM.render( <h1>Commence Dashboard Creation!</h1>, document.querySelector('main') );
+class Left extends React.Component {
+    render(){
+        return(
+            <div className="leftS">
+                <h1>Blah</h1>
+            </div>
+        )
+    }
+    }
+
+
+class TopM extends React.Component {
+    render() {
+        return(
+            <div className="topM">
+                <div className ="topM1">
+                    <h1>Blah</h1>
+                </div>
+                <div className="topM2">
+                    <h1>Blah</h1>
+                </div>
+                <div className="topM3">
+                    <h1>Blah</h1>
+                </div>
+            </div>
+        )
+    }
+}
+
+class BottomM extends React.Component {
+    render() {
+        return (
+            <div className=" bottomM">
+
+            </div>
+        )
+    }
+}
+
+class App extends React.Component {
+    render () {
+        return(
+            <div className="overallDiv"> 
+                <Left />
+                <TopM />
+            </div>
+        )
+    }
+}
+
+
+ReactDOM.render( <App/> , document.querySelector('main') );
